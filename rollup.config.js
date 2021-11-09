7dc1235980780d959da2dfc0051b94ffbafdb0d2
Daniel Rice <Daniel.L.Rice@gmail.com>
2021-11-09 10:57:36 +0100

    Use dist for out. Replace package-lock.json with yarn.lock

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,12 @@
-/**
- * @license
- * Copyright 2018 Google LLC
- * SPDX-License-Identifier: BSD-3-Clause
- */
-
 import summary from 'rollup-plugin-summary';
 import {terser} from 'rollup-plugin-terser';
 import resolve from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
 
 export default {
-  input: 'my-element.js',
+  input: 'dist/my-element.js',
   output: {
-    file: 'my-element.bundled.js',
+    file: 'dist/my-element.bundled.js',
     format: 'esm',
   },
   onwarn(warning) {
@@ -24,7 +18,7 @@ export default {
     replace({'Reflect.decorate': 'undefined'}),
     resolve(),
     terser({
-      ecma: 2017,
+      ecma: 2018,
       module: true,
       warnings: true,
       mangle: {
